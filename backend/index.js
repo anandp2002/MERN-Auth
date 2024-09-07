962d01b96e44656d98d612c43a1448459244594f
Anand P <anandpamegoze@gmail.com>
2024-09-07 16:39:26 +0530

    setup auth controller

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,12 @@ import express from 'express';
 import { configDotenv } from 'dotenv';
 
 import { connectDB } from './db/connectDB.js';
+import authRoutes from './routes/auth.route.js';
 
 configDotenv();
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello');
-});
+app.use('/api/auth', authRoutes);
 
 app.listen(3000, () => {
   connectDB();
